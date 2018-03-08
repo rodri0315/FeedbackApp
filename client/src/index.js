9cd0407134eaaecc540a88edbacfba0e396ac0b1
Jorge Rodriguez <rodri0315@gmail.com>
2018-03-08 16:52:45 -0500

    Add redux-thunk, fetch user using Async-Await.

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk'
 
 import App from './components/App';
 import reducers from './reducers';
 
-const store = createStore(reducers, {}, applyMiddleware());
+import 'materialize-css/dist/css/materialize.min.css';
+
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
   <Provider store={store}><App /></Provider>,
